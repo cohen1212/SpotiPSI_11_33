f5d8661a681c5f85591a02bc778e2cfc397e78a1
ilay <ilayco2000@gmail.com>
2026-03-17 16:45:58 +0200

    player(footer) 33

--- SpotiPSI/src/App.tsx
+++ SpotiPSI/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import MainSection from './components/header/mainSection/MainSection'
+import Player from './components/player/Player'
 
 const App = () => {
   return (
     <>
       <MainSection />
+    <Player/>
     </>
   )
 }
